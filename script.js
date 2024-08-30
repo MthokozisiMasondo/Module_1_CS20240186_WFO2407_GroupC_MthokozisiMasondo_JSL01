8413383d41cd600fae7a6022c6c40a44d081f29c
Mthokozisi Masondo <m.masondo2001@gmail.com>
2024-08-30 13:55:49 +0200

    added conditional for valid input syntax

--- script.js
+++ script.js
@@ -5,8 +5,17 @@ function validateSyntax() {
 
     // TODO: Write your validation logic here
         // Check if input starts with 'pet_' and followed by alphanumeric characters
-
+    if (
+        input.startsWith('pet_') &&
+        !isNaN(input.slice(4, 8)) &&
+        input.slice(4, 8).length === 4 &&
+        input.slice(8).length > 0 
+    ) {
+            result = "Valid Syntax 🟢"
+      } else {
+            result = "Invalid Syntax 🔴"
+         }
             document.getElementById('result').innerText = result;
-}
+    }
 
 
